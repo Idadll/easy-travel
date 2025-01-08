8536197efef08ea0436d019689f00365d832d636
Your Name <you@example.com>
2025-01-08 23:27:07 +0330

    made new changes

---
+++ script4.js
@@ -0,0 +1,38 @@
+const player5 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+const player6 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+const player7 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+const player8 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
---
+++ script5.js
@@ -0,0 +1,82 @@
+const player9 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+const player10 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+const player11 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+const player12 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+
+const player5 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+const player6 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+
+
+
+
+
+const player7 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
+
+
+
+
+const player8 = document.getElementById('myLottie');
+function playAnimation() { player.play(); } 
+
+function stopAnimation() { player.stop(); } 
+function setSpeed(speed) { player.setSpeed(speed); }
